e3c58ab640f0ac2b0b5fc7becd47012a3996483b
Nimesh Kiran Verma <nimesh.aug11@gmail.com>
2017-05-27 15:06:06 +0530

    Updated the model with deviations

--- UpwardsUserEventsData/ProcessEventDataDynamoRds/service/event_data_process_service.py
+++ UpwardsUserEventsData/ProcessEventDataDynamoRds/service/event_data_process_service.py
@@ -1,4 +1,5 @@
 import datetime
+from difflib import SequenceMatcher
 from database_service import Database
 from abc import ABCMeta, abstractmethod
 
@@ -194,6 +195,138 @@ class ProcessedEventsData(object):
                 'update': {}
             }
         }
+        self.__field_values = {
+            'aadhaar_details': {
+                'create': {
+                    "start_value": {},
+                    "end_value": {},
+                },
+                'update': {
+                    "start_value": {},
+                    "end_value": {},
+                }
+            },
+            'aadhaar': {
+                'create': {
+                    "start_value": {},
+                    "end_value": {},
+                },
+                'update': {
+                    "start_value": {},
+                    "end_value": {},
+                }
+            },
+            'bank': {
+                'create': {
+                    "start_value": {},
+                    "end_value": {},
+                },
+                'update': {
+                    "start_value": {},
+                    "end_value": {},
+                }
+            },
+            'documents': {
+                'create': {
+                    "start_value": {},
+                    "end_value": {},
+                },
+                'update': {
+                    "start_value": {},
+                    "end_value": {},
+                }
+            },
+            'education': {
+                'create': {
+                    "start_value": {},
+                    "end_value": {},
+                },
+                'update': {
+                    "start_value": {},
+                    "end_value": {},
+                }
+            },
+            'eligibility_review': {
+                'create': {
+                    "start_value": {},
+                    "end_value": {},
+                },
+                'update': {
+                    "start_value": {},
+                    "end_value": {},
+                }
+            },
+            'finance': {
+                'create': {
+                    "start_value": {},
+                    "end_value": {},
+                },
+                'update': {
+                    "start_value": {},
+                    "end_value": {},
+                }
+            },
+            'kyc_review': {
+                'create': {
+                    "start_value": {},
+                    "end_value": {},
+                },
+                'update': {
+                    "start_value": {},
+                    "end_value": {},
+                }
+            },
+            'loan_product': {
+                'create': {
+                    "start_value": {},
+                    "end_value": {},
+                },
+                'update': {
+                    "start_value": {},
+                    "end_value": {},
+                }
+            },
+            'pan': {
+                'create': {
+                    "start_value": {},
+                    "end_value": {},
+                },
+                'update': {
+                    "start_value": {},
+                    "end_value": {},
+                }
+            },
+            'personal_contact': {
+                'create': {
+                    "start_value": {},
+                    "end_value": {},
+                },
+                'update': {
+                    "start_value": {},
+                    "end_value": {},
+                }
+            },
+            'profession': {
+                'create': {
+                    "start_value": {},
+                    "end_value": {},
+                },
+                'update': {
+                    "start_value": {},
+                    "end_value": {},
+                }
+            },
+            'signup': {
+                'create': {
+                    "start_value": {},
+                    "end_value": {},
+                },
+                'update': {
+                    "start_value": {},
+                    "end_value": {},
+                }
+            }
+        }
         self.__process_raw_data()
         self.sql_queries = self.__get_sql_queries()
 
@@ -202,6 +335,15 @@ class ProcessedEventsData(object):
 
     def __get_mode(self, event):
         return event['M']['data']['M']['field_events']['L'][0]['M']['data']['M']['mode']['S']
+
+    def __update_field_value(self, field_event, screen):
+        field_name = field_event['M']['name']['S']
+        value = str(field_event['M']['data']['M']['value'].values()[0])
+        mode = field_event['M']['data']['M']['mode']['S']
+        if not field_name in self.__field_values[screen][mode]['start_value']:
+            self.__field_values[screen][mode][
+                'start_value'][field_name] = value
+        self.__field_values[screen][mode]['end_value'][field_name] = value
 
     def __process_raw_data(self):
         for screen_event_data in self.__raw_data['data']['L']:
@@ -217,6 +359,7 @@ class ProcessedEventsData(object):
                     self.__field_data[screen][mode][field_name] += 1
                 else:
                     self.__field_data[screen][mode][field_name] = 1
+                self.__update_field_value(field_event, screen)
 
     def __get_screen_sql_query(self):
         sql_query = """ INSERT INTO analytics_screen_eventdata 
@@ -235,18 +378,23 @@ class ProcessedEventsData(object):
         sql_query = sql_query[:-1] + ' ;'
         return sql_query
 
+    def __get_deviation(self, a, b):
+        return (1 - SequenceMatcher(None, a, b).ratio()) * 100
+
     def __get_field_sql_query(self):
         sql_query = """ INSERT INTO analytics_field_eventdata 
-                        (customer_id, field, edits, screen, mode, created_at, updated_at, is_active) 
+                        (customer_id, field, edits, deviation, screen, mode, created_at, updated_at, is_active) 
                         VALUES 
                     """
         for screen, data in self.__field_data.iteritems():
             for mode, mode_data in data.iteritems():
                 for field, edits in mode_data.iteritems():
-                    sql_query += """({customer_id}, '{field}', {edits}, '{screen}', '{mode}', ( select now()::timestamp with time zone at time zone 'Asia/Kolkata'), ( select now()::timestamp with time zone at time zone 'Asia/Kolkata'), TRUE) ,""".format(
+                    sql_query += """({customer_id}, '{field}', {edits}, {deviation}, '{screen}', '{mode}', ( select now()::timestamp with time zone at time zone 'Asia/Kolkata'), ( select now()::timestamp with time zone at time zone 'Asia/Kolkata'), TRUE) ,""".format(
                         customer_id=self.customer_id,
                         field=field,
                         edits=edits,
+                        deviation=self.__get_deviation(self.__field_values[screen][mode]['start_value'][
+                                                       field], self.__field_values[screen][mode]['end_value'][field]),
                         screen=screen,
                         mode=mode,
                     )
